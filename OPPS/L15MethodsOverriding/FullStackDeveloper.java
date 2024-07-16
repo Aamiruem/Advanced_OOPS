f88b92ad390f7ae2659d8d7319b87a656420bd78
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-16 21:56:38 +0530

    commit add

--- OPPS/L15MethodsOverriding/FullStackDeveloper.java
+++ OPPS/L15MethodsOverriding/FullStackDeveloper.java
@@ -6,5 +6,6 @@ public class FullStackDeveloper extends FrontEndDeveloper {
         System.out.println("My skill is Full Stack Developer");
 
         // System.out.println("I can code with I know frontEnd part using HTML, CSS, JS, REACT, JAVA, I know backend part using NODEJS, MONGODB, EXPRESS");
+        System.out.println();
     }
 }
